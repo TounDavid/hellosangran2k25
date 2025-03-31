100a25bd787123deae02ba19e5bc9b05d20f26ae
TounDavid <vitrith74@gmail.com>
2025-04-01 00:26:08 +0700

    Add files via upload

---
+++ idex.html.js
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>College CoST</title>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="https://fonts.googleeapis.com/css?family=Roboto:wght@400;700&disoay=swap" rel="stylesheet">
+      <link rel="icon" type="image" href="https://scontent.fpnh2-2.fna.fbcdn.net/v/t39.30808-6/486311875_1936528630535674_6488388651892887062_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeHHVNao3gyy4lOIQ7AEqhMhAdNNZVk4QVEB001lWThBUfxbEN9-CRlG6a61tlWd9Hz7ahfIa9Pt7WIXOrgISsaS&_nc_ohc=cecLXiWg9kUQ7kNvgGQvd9G&_nc_oc=AdnFGSP2s3-bohE9MxkAYYJT0shqUerNeJJ84lG9d0PgvIgc966Zp_E7AKWqLBh9bw0&_nc_zt=23&_nc_ht=scontent.fpnh2-2.fna&_nc_gid=ffumdthgzUGy5Aj3aepLsA&oh=00_AYEUsBEjEvC7dj6xcdt4m41NEmTVtG7KAlzo4mqn3hY6Ng&oe=67E6E300">
+<link rel="stylesheet" href="styles.css">
+<style>
+img {
+    border: 4px solid darkblue;
+}
+</style>
+</head>
+<body>
+<img src="https://cost.uc.edu.kh/images/banner.jpg" alt="images" style="width: 2580px;height:300px;">
+<h1 style="color: white;background-color: darkblue;"><marquee behavior="scroll" direction="right" scrollamount="8"><img src="images/cost.jpg" alt="images" style="width:130px;height:50px;">​ស្វាគមន៍!🌺​ welcome🌸 College of Science and Technology</imag></marquee></h1>
+<img src="images/da1.jpg" alt="images" style="width:490px;height:350px;"><>
+<img src="images/da2.jpg" alt="images" style="width:490px;height:350px;">
+<img src="images/da3.jpg" alt="images" style="width:490px;height:350px;">
+<p>
+Welcome to College of Science and Technology of The University of Cambodia. For everyone who wishes to pursue for any AA and BA degrees of the study programs, please register now or visit the campus of The University of Cambodia. College of Science and Technology at The University of Cambodia. The College of Science and Technology (CoST) was established at the same time as the The University of Cambodia in 2013. The College have been developed and transforms with the rise of Science and technology in the 21st century. We drive and inspire our students to value creativity and innovation in the information technology world. The college offers undergraduate and graduate programs such as Computer Science, Information Technology and Electronics and Telecommunication. Our programs prepare students for careers in these disciplines as scientific and ICT professional. In order to meet increasing demand for graduates in computer science and information technology, we have: developed robust and modern curricula; established sophisticated facilities; and hired high qual...
+</p> 
+<h2 style="color: white;background-color: darkblue;"> Majors offering in College of science and Technology</h2>
+<head>
+    <style>
+        table,
+        th,
+        td {
+            border: 1px solid blue;
+        }
+    </style>
+</head>
+<table style="width: 100%;">
+    <tr>
+        <th>Majors</th>
+        <th>Associate,Bachelor</th>
+        <th>Master , PhD</th>
+    </tr>
+    <tr>
+        <td>Computer Science (offering)</td>
+        <td>AA and BA</td>
+        <td>MS and PhD</td>
+    </tr>
+    <tr>
+        <td>Information Technology (offering)</td>
+        <td>BA</td>
+        <td>MS</td>
+    </tr>
+</tr>
+<tr>
+    <td>Electronics and Telecommunications</td>
+    <td>BA</td>
+    <td>MS</td>
+</tr>
+</table>
+<h2 style="color:white;background-color:darkblue;"> College of Science and Technology Computer Labs</h2>
+<img src="images/da4.jpg" alt="images" style="width:490px;height:350px;">
+<img src="images/da5.jpg" alt="images" style="width:490px;height:350px;">
+<img src="images/da6.jpg" alt="images" style="width:490px;height:350px;">
+<table style="width: 100%; border-color:#ebf5fb;">
+    <caption>Computer Labs University OF Cambodia</caption>
+    <tr>
+        <th>Multimedia Lab</th>
+        <th>E-learning Lab</th>
+        <th>Networking Lab</th>
+        <th>IT-Center Lab</th>
+    </tr>
+    <tr>
+        <td>Geaphic Design</td>
+        <td>Graphic Design</td>
+        <td>NetworkingDesign</td>
+        <td>Computer Application</td>
+    </tr>
+    <tr>
+        <td>Computer Application</td>
+        <td>Produce video contents</td>
+        <td>Web Development</td>
+        <td>Computer Programming</td>
+    </tr>
+    <tr>
+        <td>Mobile Application</td>
+        <td>Computer Application</td>
+        <td>Network Security</td>
+        <td>Databace Development</td>
+    </tr>
+
+</table>
+ <img src="images/icon.jpg" width="50" heilght="50">
+ College Science and Technology Coppyright@2024
+ </footer>
+
+</body> 
+<footer>
+</html>
+<P> TOUN DAVID</p>
